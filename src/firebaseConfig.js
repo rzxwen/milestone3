013a280d138d7cd61e8148c5e1eeaf644702f56f
Rico Wen <ricow@sfu.ca>
2025-03-31 19:14:32 -0700

    ttt
    ttt

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,11 @@
 //initialize a firebase app in the react native project
 import { initializeApp } from "firebase/app";
-
-//import firebase authenticiation functionality into your project
-import { getAuth } from "firebase/auth";
-
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { getFirestore } from "firebase/firestore";
 
-//your web app's firebase configuration
+//your web app's firebase configurationr
 const firebaseConfig = {
   apiKey: "AIzaSyBuSnpUij8H5Z3vukCWvuOe4dRnap87fl8",
   authDomain: "iat-359---final-proj.firebaseapp.com",
@@ -15,12 +13,15 @@ const firebaseConfig = {
   storageBucket: "iat-359---final-proj.firebasestorage.app",
   messagingSenderId: "584307728520",
   appId: "1:584307728520:web:0bcfcaeaac7bca45c23f3d"
-
 };
 
 //exports the app so that it can be used in other parts of the project
-export const firebase_app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-//exports the firebase authentication instance to be used in other parts of your project
-export const firebase_auth = getAuth(firebase_app);
-export const firestore_db = getFirestore(firebase_app);
+// Initialize Auth with persistence
+export const firebase_auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+//exports the firestore database instance to be used in other parts of your project
+export const firestore_db = getFirestore(app);
